a4319d8d4dfbf6870f2067cdbc8e35fe272387f4
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-30 02:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+ const initialTime = Date.now();
+
+    function createPromise(name) {
+      const delay = Math.floor(Math.random() * 3 + 1) * 1000; // random 1-3 sec
+      const start = Date.now();
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const timeTaken = ((Date.now() - start) / 1000).toFixed(3);
+          resolve({ name, time: timeTaken });
+        }, delay);
+      });
+    }
+
+    Promise.all([createPromise("Promise 1"), createPromise("Promise 2"), createPromise("Promise 3")])
+      .then((results) => {
+        const tbody = document.getElementById("output");
+        tbody.innerHTML = ""; // remove Loading row
+
+        results.forEach((res) => {
+          tbody.innerHTML += `<tr>
+                                <td>${res.name}</td>
+                                <td>${res.time}</td>
+                              </tr>`;
+        });
+
+        const totalTime = ((Date.now() - initialTime) / 1000).toFixed(3);
+        tbody.innerHTML += `<tr>
+                              <td>Total</td>
+                              <td>${totalTime}</td>
+                            </tr>`;
+      })
+      .catch((err) => console.error("Error:", err));
